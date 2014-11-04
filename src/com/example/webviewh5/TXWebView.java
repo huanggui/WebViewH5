b3c9c05a934cd68bdc3420516fb88617613134bd
Administrator <Administrator@ighuang-PC1.tencent.com>
2014-11-04 14:46:12 +0800

    add TXWebView.

---
+++ src/com/example/webviewh5/TXWebView.java
@@ -0,0 +1,16 @@
+package com.example.webviewh5;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.webkit.WebView;
+
+public class TXWebView extends WebView{
+	public TXWebView(Context context) {
+		super(context);
+	}
+
+	public TXWebView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+}
